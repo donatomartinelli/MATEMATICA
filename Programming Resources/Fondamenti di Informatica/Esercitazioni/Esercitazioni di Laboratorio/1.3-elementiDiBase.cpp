baba84aaed3a9b719bcbced5ad14aa247126eb8f
Donato Martinelli <113515906+donatomartinelli@users.noreply.github.com>
2025-09-16 22:41:49 +0200

    Update 1.3-elementiDiBase.cpp

--- Programming Resources/Fondamenti di Informatica/Esercitazioni/Esercitazioni di Laboratorio/1.3-elementiDiBase.cpp
+++ Programming Resources/Fondamenti di Informatica/Esercitazioni/Esercitazioni di Laboratorio/1.3-elementiDiBase.cpp
@@ -1,8 +1,9 @@
-// Scrivere un programma in linguaggio C++ che legga da tastiera il nome di un mese dell’anno e stabilisca
-// - Il numero dei giorni di cui il mese è formato;
-// - stampi sullo schermo il risultato
-//  l'algoritmo è basato sul detto:"30 giorni ha novembre, con aprile, giugno e settembre, 
-// di 28 ce n'è uno (febbraio) tutti gli altri son 31 
+/* 
+Scrivere un programma in linguaggio C++ che legga da tastiera il nome di un mese dell’anno e stabilisca:
+- Il numero dei giorni di cui il mese è formato;
+- stampi sullo schermo il risultato.
+(l'algoritmo è basato sul detto:"30 giorni ha novembre, con aprile, giugno e settembre, di 28 ce n'è uno (febbraio) tutti gli altri son 31.)
+*/ 
 
 #include <iostream>
 #include <string>
@@ -10,25 +11,30 @@
 using namespace std;
 
 int main(){
-
-    int numGiorni=0;
+    int numGiorni;
     string mese;
 
-    cout << "\nInserisci nome mese: " << endl;
+    numGiorni = 0;
+    
+    cout << "Inserisci nome mese: " 
+         << endl;
+    
     getline(cin,mese);
 
-    if (mese == "novembre" || mese == "aprile" || mese == "giugno" || mese == "settembre")
+    if (mese == "novembre" || mese == "aprile" || mese == "giugno" || mese == "settembre") {
         numGiorni = 30;
-    else if (mese == "febbraio")
+    } else if (mese == "febbraio") {
         numGiorni = 28;
-    else
+    } else {
         numGiorni = 31;
-    cout << "Il mese di " << mese << " ha " << numGiorni << " giorni." << endl;
+    }
     
-
-    if ((mese == "aprile") || (mese == "giugno") || (mese == "settembre") || (mese == "novembre")){numGiorni=30;}
-     else {if (mese == "febbraio") {numGiorni=28;} else {numGiorni=31;}}
-    cout << "il mese di" << " " << mese<< " " << "ha" <<" " << numGiorni << " " << "giorni"<< endl;
-
+    cout << "Il mese di " 
+         << mese 
+         << " ha " 
+         << numGiorni 
+         << " giorni." 
+         << endl;
+    
     return 0;
 }
