6af48619e89672d0cc2f5a30474eec46d60c81cd
Donato Martinelli <113515906+donatomartinelli@users.noreply.github.com>
2025-09-16 22:34:55 +0200

    Update 1.2-elementiDiBase.cpp

--- Programming Resources/Fondamenti di Informatica/Esercitazioni/Esercitazioni di Laboratorio/1.2-elementiDiBase.cpp
+++ Programming Resources/Fondamenti di Informatica/Esercitazioni/Esercitazioni di Laboratorio/1.2-elementiDiBase.cpp
@@ -1,9 +1,10 @@
-// Utilizzando un editor di testo creare nella cartella di lavoro un file denominato “dati.txt”, contenente tre numeri (es: 20 45.4 15.74)
-// Modificare il programma C++ scritto per risolvere l’esercizio n.1 in modo da effettuare le seguenti
-// operazioni:
-// a. Acquisire i valori delle basi e dell’altezza del trapezio dal file “dati.txt” creato al passo precedente
-// b. Calcolare il valore dell’area del trapezio
-// c. Scrivere il valore dell’area calcolata in un nuovo file denominato “risultati.txt”.
+/* 
+Utilizzando un editor di testo creare nella cartella di lavoro un file denominato “dati.txt”, contenente tre numeri (es: 20 45.4 15.74)
+Modificare il programma C++ scritto per risolvere l’esercizio n.1 in modo da effettuare le seguenti operazioni:
+- Acquisire i valori delle basi e dell’altezza del trapezio dal file “dati.txt” creato al passo precedente
+- Calcolare il valore dell’area del trapezio
+- Scrivere il valore dell’area calcolata in un nuovo file denominato “risultati.txt”.
+*/ 
 
 #include <iostream>
 #include <fstream>
@@ -11,25 +12,32 @@
 using namespace std;
 
 int main (){
-    float altezza=0;
-    float baseMaggiore=0;
-    float baseMinore=0;
-    float areaTrapezio=0;
+    float altezza;
+    float baseMaggiore;
+    float baseMinore;
+    float areaTrapezio;
+    
+    altezza = 0;
+    baseMaggiore = 0;
+    baseMinore = 0;
+    areaTrapezio = 0;
 
     ifstream flussoLettura("dati.txt");
 
-    flussoLettura >> baseMaggiore >> baseMinore >> altezza;
-    areaTrapezio = ((baseMaggiore+baseMinore)*altezza)/2;
+    flussoLettura >> baseMaggiore 
+                  >> baseMinore 
+                  >> altezza;
+    
+    areaTrapezio = ((baseMaggiore + baseMinore) * altezza) / 2;
 
     flussoLettura.close();
 
     ofstream flussoStampa ("risultati.txt");
 
-    flussoStampa   << "Area del trapezio:    " << areaTrapezio;
+    flussoStampa   << "Area del trapezio: " 
+                   << areaTrapezio;
 
     flussoStampa.close();
 
     return 0;
 }
-
-
