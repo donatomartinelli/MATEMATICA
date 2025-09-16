445d710e9a5ca07eab59dd587c75306985538757
Donato Martinelli <113515906+donatomartinelli@users.noreply.github.com>
2025-09-16 22:53:03 +0200

    Create 2.2-struttureDiControllo.cpp

---
+++ Programming Resources/Fondamenti di Informatica/Esercitazioni/Esercitazioni di Laboratorio/2.2-struttureDiControllo.cpp
@@ -0,0 +1,34 @@
+/* 
+Scrivere un programma in linguaggio C++ che legga da tastiera un insieme di numeri reali (0 per fermarsi), 
+ne calcoli il prodotto e lo stampi a video.
+*/
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main (){
+    float numero;
+    float prodotto;
+
+    prodotto = 1;
+
+    while (true){
+        cout << "Inserisci un numero reale (0 per fermarti): " 
+             << endl;
+        cin  >> numero;
+
+        if (numero != 0){
+            prodotto *= numero;
+        } else {
+            break;
+        }
+    }
+
+    cout << "Prodotto: " 
+         << prodotto 
+         << endl;
+
+    return 0;
+}
