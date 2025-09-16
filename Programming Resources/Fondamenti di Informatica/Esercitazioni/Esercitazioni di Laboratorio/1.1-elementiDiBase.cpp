5530f8499739b35725c923a0e471eac482a915c2
Donato Martinelli <113515906+donatomartinelli@users.noreply.github.com>
2025-09-16 22:31:38 +0200

    Update and rename 1-elementiDiBase.cpp to 1.1-elementiDiBase.cpp

---
+++ Programming Resources/Fondamenti di Informatica/Esercitazioni/Esercitazioni di Laboratorio/1.1-elementiDiBase.cpp
@@ -0,0 +1,32 @@
+//  Scrivere un programma in linguaggio C++ che:
+//  -legga dalla tastiera la lunghezza delle basi e dell’altezza di un trapezio;
+//  -calcoli l’area del trapezio;
+//  -stampi sullo schermo l’area calcolata.
+
+#include <iostream>
+
+using namespace std;
+
+int main (){
+    float altezza;
+    float baseMaggiore;
+    float baseMinore;
+    float areaTrapezio;
+
+    altezza = 0;
+    baseMaggiore = 0;
+    baseMinore = 0;
+    areaTrapezio = 0;
+  
+    cout << "Digita rispettivamente altezza e lunghezza delle basi maggiore e minore del trapezio: ";
+    cin >> altezza 
+        >> baseMaggiore 
+        >> baseMinore;
+  
+    areaTrapezio = ((baseMaggiore + baseMinore) * altezza) / 2;
+  
+    cout << "Area del trapezio: " 
+         << areaTrapezio;
+  
+    return 0;
+}
