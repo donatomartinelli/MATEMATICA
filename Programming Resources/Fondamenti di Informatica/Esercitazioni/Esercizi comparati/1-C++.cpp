93186200ca9d3972c57bb6b15f47d41e0c3ff9d6
Donato Martinelli <113515906+donatomartinelli@users.noreply.github.com>
2025-09-17 00:07:47 +0200

    Create 1-C++.cpp

---
+++ Programming Resources/Fondamenti di Informatica/Esercitazioni/Esercizi comparati/1-C++.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+using namespace std;
+
+const int N = 5;
+
+void letturaMatrice(int matrice[N][N]) { 
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cout << matrice[i][j] << " ";
+        }
+      
+        cout << endl;   
+    }
+    
+    return;
+  
+    }
+
+int ricercaMinimo(int vettore[]) {
+    int pos = -1;
+    
+    for (int i = 0; i < N; i++) {
+        if (vettore[i] > 0) {
+            if (pos == -1 || vettore[i] < vettore[pos]) {
+                pos = i;
+            }
+        }
+    }
+    
+    return pos;
+    
+    
+void matriceFreccia(int vettore[N], int matrice[N][N]) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (i == 0) {
+                matrice[i][j] = vettore[j];
+            } else if (j == 0) {
+                matrice[i][j] = vettore[i];
+            } else if (i == j) {
+                matrice[i][j] = vettore[i];
+            } else {
+                matrice[i][j] = 0;
+            }
+        }
+    }
+    
+    letturaMatrice(matrice);
+    
+    ricercaMinimo(vettore);
+    
+    if (ricercaMinimo(vettore) != -1) {
+        cout << "Minimo tra gli elementi positivi: " << vettore[ricercaMinimo(vettore)] << endl;
+    } else {
+        cout << "Nessun elemento positivo trovato nel vettore." << endl;
+    }
+    
+    return ;
+}
+
+int main()
+{
+    int vettore[N] = {-11, 2, -1, 1, -30};
+    int matrice[N][N];
+    
+    matriceFreccia(vettore, matrice);
+    
+    return 0;
+}
