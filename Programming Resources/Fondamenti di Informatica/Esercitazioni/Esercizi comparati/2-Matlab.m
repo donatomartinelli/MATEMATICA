8c6c223d37f2629b49c27327299691a7f65da759
Donato Martinelli <113515906+donatomartinelli@users.noreply.github.com>
2025-09-17 00:04:17 +0200

    Update 2-Matlab.m

--- Programming Resources/Fondamenti di Informatica/Esercitazioni/Esercizi comparati/2-Matlab.m
+++ Programming Resources/Fondamenti di Informatica/Esercitazioni/Esercizi comparati/2-Matlab.m
@@ -1,7 +1,6 @@
 v = [1 2 3 4 6 8 29];
 w = [7 7 7 0 0 -1];
 
-
 [mat, avg, elem] = matTrig(v, w);
 
 disp("Matrice: ")
@@ -13,9 +12,9 @@ function [matrice, avg, elem] = matTrig(vec1, vec2)
     n = length(vec1);
     A = zeros(n);
     
-    A(1:n+1:end) = vec1;
-    A(n+1:n+1:end) = vec2;
-    A(2:n+1:end) = -vec2;
+    A(1 : n+1 : end) = vec1;
+    A(n+1 : n+1 : end) = vec2;
+    A(2 : n+1 : end) = -vec2;
     
     avgV = mean(vec1);
     elemMaggAvgV = length(A(A > avgV));
@@ -23,4 +22,5 @@ function [matrice, avg, elem] = matTrig(vec1, vec2)
     matrice = A;
     avg = avgV;
     elem = elemMaggAvgV;
+    
 end 
