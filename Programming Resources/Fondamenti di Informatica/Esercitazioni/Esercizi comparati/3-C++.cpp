a58d6bf1283a1b0812d6f27fe46e819169c7d67f
Donato Martinelli <113515906+donatomartinelli@users.noreply.github.com>
2025-09-17 15:49:22 +0200

    Create 3-C++.cpp

---
+++ Programming Resources/Fondamenti di Informatica/Esercitazioni/Esercizi comparati/3-C++.cpp
@@ -0,0 +1,16 @@
+/*
+    Scrivere una function in linguaggio C++ che preveda in input 4 numeri reali x, y, z e t ed effettui le seguenti operazioni:
+    - costruisca la matrice quadrata A che abbia tutti gli elementi al di sotto della diagonale principale coincidenti con x e tutti gli elementi sulla diagonale principale coincidenti con y;
+    - costruisca la matrice quadrata B che abbia tutti gli elementi al di sopra della diagonale principale coincidenti con z e tutti gli elementi sulla diagonale principale coincidenti con t;
+    - calcoli la somma C delle matrici A e B;
+    - restituisca la matrice C.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    return 0;
+}
