2e8c70c32d6a8a9760ed479f884977b76a455c36
Donato Martinelli <113515906+donatomartinelli@users.noreply.github.com>
2025-09-17 16:22:04 +0200

    Update 3-C++.cpp

--- Programming Resources/Fondamenti di Informatica/Esercitazioni/Esercizi comparati/3-C++.cpp
+++ Programming Resources/Fondamenti di Informatica/Esercitazioni/Esercizi comparati/3-C++.cpp
@@ -10,7 +10,113 @@
 
 using namespace std;
 
+const int N = 4;
+
+void letturaVettore (int v[N]) {
+    cout << "Inserisci gli elementi x, y, z, e t rispettivamente: "
+         << endl;
+
+    for (int i = 0; i < N; i++) {
+        cin  >> v[i];
+    }
+
+    cout << endl;
+
+    return ;
+}
+
+void letturaMatA (int mat[N][N], int v[N]) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (i == j) {
+                mat[i][j] = v[1];
+            } else {
+                mat[i][j] = 0;
+            }
+        }
+    }
+    
+    for (int i = 0; i < N - 1; i++) {
+        for (int j = 0; j < N - 1; j++) {
+            if (i == j) {
+                mat[i + 1][j] = v[0];
+            }
+        }
+    }
+
+    cout << endl;
+
+    return ;
+}
+
+void letturaMatB (int mat[N][N], int v[N]) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (i == j) {
+                mat[i][j] = v[3];
+            } else {
+                mat[i][j] = 0;
+            }
+        }
+    }
+    
+    for (int i = 0; i < N - 1; i++) {
+        for (int j = 0; j < N - 1; j++) {
+            if (i == j) {
+                mat[i][j + 1] = v[2];
+            }
+        }
+    }
+
+    cout << endl;
+
+    return ;
+}
+
+void sommaMat (int mat1[N][N], int mat2[N][N], int mat3[N][N]) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            mat3[i][j] = mat1[i][j] + mat2[i][j];
+        }
+    }
+
+    return ;
+}
+
+void stampaMat (int mat[N][N]) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cout << mat[i][j] << " ";
+        }
+
+        cout << endl;
+    }
+
+    cout << endl;
+
+    return ;
+}
+
 int main() {
+    int num[N];
+    int A[N][N];
+    int B[N][N];
+    int C[N][N];
+
+    letturaVettore(num);
+    
+    letturaMatA(A,num);
+    letturaMatB(B,num);
+    
+    cout << "A = " << endl;
+    stampaMat(A);
+    cout << "B = " << endl;
+    stampaMat(B);
+
+    sommaMat(A, B, C);
+
+    cout << "C = A + B = " << endl;
+    stampaMat(C);
 
     return 0;
 }
