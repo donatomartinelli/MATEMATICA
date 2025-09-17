2236c4d205fc5c5e82bcf90eadbe33438d2e5b92
Donato Martinelli <113515906+donatomartinelli@users.noreply.github.com>
2025-09-17 15:47:52 +0200

    Update 3-Matlab.m

--- Programming Resources/Fondamenti di Informatica/Esercitazioni/Esercizi comparati/3-Matlab.m
+++ Programming Resources/Fondamenti di Informatica/Esercitazioni/Esercizi comparati/3-Matlab.m
@@ -1,4 +1,4 @@
-% Scrivere una function in linguaggio Matlab e un programma in linguaggio C++ che preveda in input 4 numeri reali x, y, z e t ed effettui le seguenti operazioni:
+% Scrivere una function in linguaggio Matlab che preveda in input 4 numeri reali x, y, z e t ed effettui le seguenti operazioni:
 % - costruisca la matrice quadrata A che abbia tutti gli elementi al di sotto della diagonale principale coincidenti con x e tutti gli elementi sulla diagonale principale coincidenti con y;
 % - costruisca la matrice quadrata B che abbia tutti gli elementi al di sopra della diagonale principale coincidenti con z e tutti gli elementi sulla diagonale principale coincidenti con t;
 % - calcoli la somma C delle matrici A e B;
@@ -18,8 +18,8 @@ C = A + B
 function [mat] = matTrigSup(n, num1, num2)
     A = zeros(n);
     
-    A(1 : n+1 : end) = num2; % diagonale principale
-    A(n+1 : n+1 : end) = num1; % sopradiagonale
+    A(1 : n+1 : end) = num2;     % diagonale principale
+    A(n+1 : n+1 : end) = num1;   % sopradiagonale
         
     mat = A;
 end
@@ -27,8 +27,8 @@ end
 function [mat] = matTrigInf(n, num1, num2)
     A = zeros(n);
     
-    A(1 : n+1 : end) = num2; % diagonale principale
-    A(2 : n+1 : end) = num1; % sottodiagonale
+    A(1 : n+1 : end) = num2;     % diagonale principale
+    A(2 : n+1 : end) = num1;     % sottodiagonale
         
     mat = A;
 end
