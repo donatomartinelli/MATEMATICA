a9fb94d4dedaf1aadee78d8a97b16a014ae1dab8
Donato Martinelli <113515906+donatomartinelli@users.noreply.github.com>
2025-09-22 19:11:32 +0200

    Create 1.1-Matlab.m

---
+++ Programming Resources/Fondamenti di Informatica/Esercitazioni/Esercizi Matlab/1.1-Matlab.m
@@ -0,0 +1,6 @@
+% Generare il vettore riga x e il vettore colonna y di componenti 1,2,. . . ,10 e 10,9,. . . ,1, rispettivamente. 
+% Generare inoltre il vettore colonna z contenente 11 nodi equispaziati nell’intervallo [0, 1].
+
+x = [1 : 10]
+y = [10 : -1 : 1]'
+z = linspace(0,1,11)'
