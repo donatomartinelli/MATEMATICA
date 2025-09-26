4053e5202b4821e9ed83195861bdb6a5e879d692
Donato Martinelli <113515906+donatomartinelli@users.noreply.github.com>
2025-09-26 17:31:10 +0200

    Update 1-TripleSumForSameValues.cpp

--- Programming Resources/w3resource/C++/Exercises/Basic Algorithm/1-TripleSumForSameValues.cpp
+++ Programming Resources/w3resource/C++/Exercises/Basic Algorithm/1-TripleSumForSameValues.cpp
@@ -1,1 +1,29 @@
-ciao
+// Write a C++ program to compute the sum of two given integer values. 
+// If the two values are the same, then return triple their sum.
+
+#include<iostream>
+using namespace std;
+
+// Read two integers from input
+void readNum(int &x, int &y) 
+{
+    cout << "Insert two integer values: " << endl;
+    cin >> x >> y;
+}
+
+// Return sum, triple if equal
+int checkNum(int x, int y) 
+{
+    return x == y ? 3 * (x + y) : x + y;
+}  
+
+int main() 
+{
+    int x, y; // variables
+    
+    readNum(x,y); // input
+    
+    cout << checkNum(x,y) << endl; // output
+    
+    return 0;
+}
