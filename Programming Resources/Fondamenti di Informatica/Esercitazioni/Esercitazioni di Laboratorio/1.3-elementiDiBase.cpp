db58b8a8a530c0a0535dbd93116a589c257a0143
Donato Martinelli <113515906+donatomartinelli@users.noreply.github.com>
2025-09-27 13:19:32 +0200

    Update 1.3-elementiDiBase.cpp

--- Programming Resources/Fondamenti di Informatica/Esercitazioni/Esercitazioni di Laboratorio/1.3-elementiDiBase.cpp
+++ Programming Resources/Fondamenti di Informatica/Esercitazioni/Esercitazioni di Laboratorio/1.3-elementiDiBase.cpp
@@ -1,8 +1,8 @@
 /* 
-Scrivere un programma in linguaggio C++ che legga da tastiera il nome di un mese dell’anno e stabilisca:
-- Il numero dei giorni di cui il mese è formato;
-- stampi sullo schermo il risultato.
-(l'algoritmo è basato sul detto:"30 giorni ha novembre, con aprile, giugno e settembre, di 28 ce n'è uno (febbraio) tutti gli altri son 31.)
+    Scrivere un programma in linguaggio C++ che legga da tastiera il nome di un mese dell’anno e stabilisca:
+    - Il numero dei giorni di cui il mese è formato;
+    - stampi sullo schermo il risultato.
+    (l'algoritmo è basato sul detto:"30 giorni ha novembre, con aprile, giugno e settembre, di 28 ce n'è uno (febbraio) tutti gli altri son 31.)
 */ 
 
 #include <iostream>
