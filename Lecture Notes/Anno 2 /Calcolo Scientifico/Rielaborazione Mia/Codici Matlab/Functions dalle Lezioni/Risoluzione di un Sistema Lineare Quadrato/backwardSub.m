cf52832fcbdb3cd7e7e74fdb45cf662fecb3a8d2
Donato Martinelli <113515906+donatomartinelli@users.noreply.github.com>
2026-01-23 20:37:51 +0100

    Add backwardSub function for linear systems
    Implement backward substitution for solving upper triangular systems.

---
+++ Lecture Notes/Anno 2 /Calcolo Scientifico/Rielaborazione Mia/Codici Matlab/Functions dalle Lezioni/Risoluzione di un Sistema Lineare Quadrato/backwardSub.m
@@ -0,0 +1,55 @@
+function x = backwardSub(U, b)
+% FORWARDSUB Risolve il sistema lineare triangolare superiore Ux = b usando la vettorizzazione (più veloce in esecuzione). 
+%   x = BACKWARDSUB(U, b) restituisce il vettore soluzione x.
+%
+%   INPUT:
+%       U - Matrice quadrata triangolare superiore (n x n)
+%       b - Vettore dei termini noti (n x 1)
+%
+%   OUTPUT:
+%       x - Vettore soluzione (n x 1)
+
+    %% 1. Validazione degli Input
+    arguments
+        U (:,:) double {mustBeNumeric}
+        b (:,1) double {mustBeNumeric}
+    end
+
+    %% 2. Controllo Dimensioni
+    % Ricaviamo le dimensioni
+    n = size(U, 1);
+    
+    % Verifichiamo che U sia quadrata
+    if size(U, 2) ~= n
+        error('BackwardSub:NonSquareMatrix', 'La matrice U deve essere quadrata.');
+    end
+
+    % Verifichiamo che b sia compatibile con U
+    if length(b) ~= n
+        error('BackwardSub:DimensionMismatch', 'Le dimensioni di U e b non corrispondono.');
+    end
+    
+    %% 3. Controllo Singolarità
+    % Se c'è uno zero sulla diagonale, non possiamo dividere.
+    % Usiamo una piccola tolleranza per i numeri floating point.
+    if any(abs(diag(U)) < eps)
+        error('BackwardSub:SingularMatrix', 'La matrice è singolare.');
+    end
+
+    %% 4. Algoritmo Vettorizzato
+    % Pre-allocazione
+    x = zeros(n, 1);
+
+    % Calcoliamo l'ultimo elemento manualmente per avviare il processo
+    x(n) = b(n) / U(n,n);
+    
+    for i = n-1 : -1 : 1
+        % Prendi la porzione di riga i-esima a destra della diagonale: U(i, i+1:n)
+        % Prendi la porzione di soluzione x già calcolata (che sta "sotto"): x(i+1:n)
+        % Il loro prodotto matriciale (Riga * Colonna) è la sommatoria.
+        sommatoria = U(i, i+1:n) * x(i+1:n);
+        
+        % Calcolo finale
+        x(i) = (b(i) - sommatoria) / U(i,i);
+    end
+end
