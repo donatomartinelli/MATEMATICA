71df5ddefe1d6174e6bc556d82476d514acc88f1
Donato Martinelli <113515906+donatomartinelli@users.noreply.github.com>
2026-02-04 15:22:33 +0100

    Create cifDecCorr.m

---
+++ Lecture Notes/Anno 2 /Calcolo Scientifico/Exercises/Matlab Codes/0 - Rappresentazione dei Numeri in un Calcolatore/cifDecCorr.m
@@ -0,0 +1,29 @@
+function n = cifDecCorr(valExact, valApprox)
+% CIFDECCORR Calcola il numero di cifre decimali corrette.
+%  n = CIFDECCORR(valExact, valApprox) calcola il numero di cifre 
+%  decimali significative corrette basandosi sull'errore assoluto.
+%
+%  Input:
+%    valExact  - Valore esatto (numerico: scalare, vettore o matrice)
+%    valApprox - Valore approssimato (numerico: stesse dimensioni di valExact)
+%
+%  Output:
+%    n         - Numero intero di cifre decimali corrette.
+%      Restituisce Inf se valExact e valApprox sono identici (errAbs 0).
+
+    %% 1. Validazione degli Input
+    arguments
+        valExact  {mustBeNumeric}
+        valApprox {mustBeNumeric}
+    end
+
+    %% 2. Controllo Dimensioni
+    if ~isequal(size(valExact), size(valApprox))
+        error('cifDecCorr:SizeMismatch', 'Gli input devono avere la stessa dimensione.');
+    end
+
+    %% 3. Calcolo cifre decimali corrette
+    % Calcolo l'errore assoluto
+    err = errAbs(valExact, valApprox);
+    n = floor(-log10(2 * err));
+end
