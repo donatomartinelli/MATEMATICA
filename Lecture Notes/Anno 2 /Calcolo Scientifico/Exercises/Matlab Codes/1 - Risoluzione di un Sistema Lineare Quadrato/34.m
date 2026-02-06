c90ceace81923342f419f6b04ba4e063c48beae4
Donato Martinelli <113515906+donatomartinelli@users.noreply.github.com>
2026-02-06 19:57:13 +0100

    Update 34.m

--- Lecture Notes/Anno 2 /Calcolo Scientifico/Exercises/Matlab Codes/1 - Risoluzione di un Sistema Lineare Quadrato/34.m
+++ Lecture Notes/Anno 2 /Calcolo Scientifico/Exercises/Matlab Codes/1 - Risoluzione di un Sistema Lineare Quadrato/34.m
@@ -72,3 +72,104 @@ function [L, U, rho] = factorLUr(A)
     % Calcolo finale rho: max crescita / max iniziale
     rho = max(rhos) / rhos(1);
 end
+
+
+function [L, U, P, s, rho] = factorLUPPr(A)
+% FACTORLUPPR Fattorizzazione LU con Pivoting Parziale (PA = LU).
+%  [L, U, P, s, rho] = factorLUPPr(A)
+%
+%  Input:
+%    A - Matrice dei coefficienti (n x n)
+%
+%  Output:
+%    L   - Matrice triangolare inferiore unitaria
+%    U   - Matrice triangolare superiore
+%    P   - Matrice di permutazione
+%    s   - Numero di scambi effettuati (per il determinante)
+%    rho - Fattore di crescita (stabilità numerica)
+
+    %% 1. Validazione Input
+    arguments
+        A (:,:) double {mustBeNumeric}
+    end
+
+    %% 2. Verifica Dimensioni
+    % Estraggo le dimensioni
+    [n, m] = size(A);
+
+    % Verifico se la matrice è quadrata
+    if n ~= m
+        error('factorLUPP:NonSquareMatrix', 'La matrice deve essere quadrata (Size: %dx%d).', n, m);
+    end
+
+    %% 3. Inizializzazione
+    absA = abs(A);
+    P = eye(n);      % Matrice di permutazione iniziale
+    s = 0;           % Contatore scambi
+    
+    % Inizializzazione vettore per rho
+    rhos = zeros(1,n);
+    if n > 0
+        rhos(1) = max(absA(:)); % Massimo iniziale
+    else
+        rhos(1) = 0;
+    end
+
+    %% 4. Eliminazione di Gauss con Pivoting Parziale
+    for k = 1 : n-1
+        % Pivoting parziale
+        % Trovo il massimo nella colonna k (dal pivot in giù)
+        [~, r_rel] = max(abs(A(k:n, k)));
+
+        % Converto in indice globale
+        r_glob = r_rel + k - 1;
+            
+        % Se il pivot migliore non è quello attuale, scambio
+        if r_glob ~= k
+            % Scambio righe in A (compresi i moltiplicatori precedenti)
+            A([k, r_glob], :) = A([r_glob, k], :);
+            
+            % Scambio righe in P
+            P([k, r_glob], :) = P([r_glob, k], :);
+            
+            % Aggiorno variabile s
+            s = s + 1;
+        end
+        
+        % Controllo pivot nullo (sicurezza)
+        if A(k,k) == 0
+             error('factorLUPPr:SingularMatrix', 'Matrice singolare: pivot nullo alla riga %d.', k);
+        end
+        
+        % Eliminazione
+        for i = k+1 : n
+            % Calcolo e assegnazione del moltiplicatore
+            A(i,k) = -A(i,k) / A(k,k);
+            
+            % Aggiornamento riga i-esima
+            for j = k+1:n
+                A(i,j) = A(i,j) + A(i,k) * A(k,j);
+            end
+        end
+        
+        % Calcolo rho (Massimo Locale)
+        % Estraggo sottomatrice attiva
+        tempSub = abs(A(k:n, k:n));
+        
+        % Azzero i moltiplicatori appena calcolati
+        % per misurare solo la crescita degli elementi di U.
+        tempSub(2:end, 1) = 0;
+        
+        rhos(k+1) = max(tempSub(:));
+    end
+    
+    %% 5. Costruzione Output
+    % L: Identità - parte triangolare inferiore (segno compensato)
+    L = eye(n) - tril(A, -1);
+    
+    % U: Parte triangolare superiore
+    U = triu(A);
+    
+    % Calcolo Rho finale
+    rho = max(rhos) / rhos(1);
+end
